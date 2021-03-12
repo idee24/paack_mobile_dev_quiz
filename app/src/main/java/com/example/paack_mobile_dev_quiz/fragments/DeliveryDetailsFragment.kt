eabac08c3990a006909f139ed2a95e60ebf86ce0
idee1140 <idee1140@gmail.com>
2021-03-12 21:26:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/paack_mobile_dev_quiz/fragments/DeliveryDetailsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.paack_mobile_dev_quiz.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.paack_mobile_dev_quiz.MainActivity
+import com.example.paack_mobile_dev_quiz.R
+
+/**
+ *@Created by Yerimah on 3/12/2021.
+ */
+class DeliveryDetailsFragment : Fragment(R.layout.fragment_delivery_details) {
+
+    private lateinit var context: MainActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context = activity as MainActivity
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
